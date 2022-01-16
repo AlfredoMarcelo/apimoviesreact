fa4f9a8f7ae1fa3df69d013e6b86e1f1845c56bd
alfredo <alfredo.supanta.26@gmail.com>
2022-01-15 23:22:16 -0300

    se añadio semantic-ui-react

--- frontend/react-movie/src/App.js
+++ frontend/react-movie/src/App.js
@@ -1,17 +1,17 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
   
   useEffect(()=>{
     fetch('/api/all_movies').then(response =>
       response.json().then(data => {
-        console.log(data);
+        console.log(data.movies);
       }))
-  },[])
+  },[]);
   
-  
-  
+  console.log(movies);
   return (
     <div className="App">
       HOla bebeeee
--- frontend/react-movie/src/components/Movies.js
+++ frontend/react-movie/src/components/Movies.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {List, Heading} from "semantic-ui-react"
+
 
 export const Movies = ({movies})=>{
     return(
-        <div>
-            {movies.length}
-        </div>
+        <List>
+            {movies.map(movie =>{
+                return (
+                    <List.Item key={movie.title}>
+                        <Heading>{movie.title}</Heading>
+                    </List.Item>
+                )
+            })}
+        </List>
     )
 
 }
