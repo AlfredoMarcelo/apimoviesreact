8e830db8bdc6d98d977277c1672122b8753dc81d
alfredo <alfredo.supanta.26@gmail.com>
2022-01-15 22:14:31 -0300

    conexión a base de datos exitosa

--- frontend/react-movie/src/App.js
+++ frontend/react-movie/src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
+  
+  useEffect(()=>{
+    fetch('/api/all_movies').then(response =>
+      response.json().then(data => {
+        console.log(data);
+      }))
+  },[])
+  
+  
+  
   return (
     <div className="App">
       HOla bebeeee
